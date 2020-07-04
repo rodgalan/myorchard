be7ae7a83773c29e1d39e4b3170d93fc1cb0f8dd
anna-rodriguez <anna.rodriguez@scmspain.com>
2020-07-04 22:24:27 +0200

    CreateTaskFeature red acceptance test

---
+++ src/integrationTest/java/org/happyhome/myorhcard/acceptance/CreateTaskFeature.java
@@ -0,0 +1,33 @@
+package org.happyhome.myorhcard.acceptance;
+
+import org.happyhome.myorhcard.infrastructure.MyorhcardApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+
+@SpringBootTest(classes = {MyorhcardApplication.class}, webEnvironment = RANDOM_PORT)
+public class CreateTaskFeature {
+
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @Test
+  void should_create_a_task() throws Exception {
+
+    String create_a_task_request ="{"
+        + "name : \"myTaskName\","
+        + "schedule:{date: \"2020-08-20\",time: \"14:00\"},"
+        + "description : \"this is my task description\"}";
+
+    ResponseEntity<Void> result = this.testRestTemplate.postForEntity("/task", create_a_task_request, Void.class);
+
+    assertThat(result.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+  }
+}
