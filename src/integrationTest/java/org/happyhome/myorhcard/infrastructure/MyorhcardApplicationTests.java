b692acb299389482511203459aefd4168ef62aaf
anna-rodriguez <anna.rodriguez@scmspain.com>
2020-07-05 11:49:45 +0200

    applicationIsUp green integration test

--- src/integrationTest/java/org/happyhome/myorhcard/infrastructure/MyorhcardApplicationTests.java
+++ src/integrationTest/java/org/happyhome/myorhcard/infrastructure/MyorhcardApplicationTests.java
@@ -1,13 +1,25 @@
 package org.happyhome.myorhcard.infrastructure;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class MyorhcardApplicationTests {
 
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	void applicationIsUp() {
+		ResponseEntity<String> response = testRestTemplate.getForEntity("/actuator/health", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
 }
