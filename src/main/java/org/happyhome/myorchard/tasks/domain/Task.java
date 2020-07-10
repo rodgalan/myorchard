1103db50879cb40c4876e59c42c9bffa2767d7a1
anna-rodriguez <anna.rodriguez@scmspain.com>
2020-07-10 19:32:06 +0200

    Task should generate a unique id

--- src/main/java/org/happyhome/myorchard/tasks/domain/Task.java
+++ src/main/java/org/happyhome/myorchard/tasks/domain/Task.java
@@ -1,13 +1,21 @@
 package org.happyhome.myorchard.tasks.domain;
 
+import java.util.UUID;
+
 public class Task {
   private final Name name;
   private final Description description;
   private final TaskCalendar calendar;
+  private final UUID uuid;
 
   public Task(Name name, Description description, TaskCalendar calendar) {
     this.name = name;
     this.description = description;
     this.calendar = calendar;
+    this.uuid = UUID.randomUUID();
+  }
+
+  public UUID getUid() {
+    return uuid;
   }
 }
